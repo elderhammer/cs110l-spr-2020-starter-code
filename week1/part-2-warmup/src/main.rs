9bb662af4fc4799dc78a1a50a38943398d363b05
elderhammer <81279860+elderhammer@users.noreply.github.com>
2024-01-10 13:01:14 +0800

    finish week1

--- week1/part-2-warmup/src/main.rs
+++ week1/part-2-warmup/src/main.rs
@@ -1,21 +1,84 @@
 /* The following exercises were borrowed from Will Crichton's CS 242 Rust lab. */
 
 use std::collections::HashSet;
+use std::vec::Vec;
 
 fn main() {
     println!("Hi! Try running \"cargo test\" to run tests.");
 }
 
+/// Implement add_n, which takes a vector of numbers and some number n.
+/// The function should return a new vector whose elements are the numbers
+/// in the original vector v with n added to each number.
 fn add_n(v: Vec<i32>, n: i32) -> Vec<i32> {
-    unimplemented!()
+    // unimplemented!()
+
+    // ❌ 没审题
+    // let mut v = v;
+    // for number in v.iter_mut() {
+    //     *number += n;
+    // }
+    // v
+
+    // ✅
+    let mut nv = Vec::new();
+    for number in v {
+        nv.push(number + n);
+    }
+    nv
 }
 
+/// Implement add_n_inplace, which does the same thing as add_n,
+/// but modifies v directly (in place) and does not return anything.
 fn add_n_inplace(v: &mut Vec<i32>, n: i32) {
-    unimplemented!()
+    // unimplemented!()
+
+    // 🚧 需要额外生成一个迭代器
+    // for number in v.iter_mut() {
+    //     *number += n;
+    // }
+
+    // ✅ 只需 range + 下标访问
+    for i in 0..v.len() {
+        v[i] = v[i] + n;
+    }
 }
 
+/// Implement dedup that removes duplicate elements from a vector in-place (i.e. modifies v directly).
+/// If an element is repeated anywhere in the vector, you should keep the element that appears first.
+/// You may want to use a HashSet for this.
 fn dedup(v: &mut Vec<i32>) {
-    unimplemented!()
+    // unimplemented!()
+
+    // 🚧 remove 耗时 O(n^2)
+    // let mut numbers: HashSet<i32> = HashSet::new();
+    // let mut idxs: Vec<i32> = Vec::new();
+    // for i in 0..v.len() {
+    //     if numbers.contains(&v[i]) {
+    //         idxs.push(i as i32);
+    //     } else {
+    //         numbers.insert(v[i]);
+    //     }
+    // }
+    // let mut j = 0i32;
+    // for i in 0..idxs.len() {
+    //     v.remove((idxs[i] - j) as usize);
+    //     j = j + 1;
+    // }
+
+    // 相比于上面的逻辑，用空间换了时间
+    let mut numbers: HashSet<i32> = HashSet::new();
+    let mut nvec: Vec<i32> = Vec::new();
+    for i in 0..v.len() {
+        if !numbers.contains(&v[i]) {
+            numbers.insert(v[i]);
+            nvec.push(v[i]);
+        }
+    }
+    v.clear();
+    for i in 0..nvec.len() {
+        v.push(nvec[i]);
+    }
 }
 
 #[cfg(test)]
--- week1/part-3-hangman/src/main.rs
+++ week1/part-3-hangman/src/main.rs
@@ -17,6 +17,7 @@ use rand::Rng;
 use std::fs;
 use std::io;
 use std::io::Write;
+use std::collections::HashSet;
 
 const NUM_INCORRECT_GUESSES: u32 = 5;
 const WORDS_PATH: &str = "words.txt";
@@ -37,4 +38,107 @@ fn main() {
     // println!("random word: {}", secret_word);
 
     // Your code here! :)
+
+    println!("Welcome to CS110L {}!", secret_word);
+
+    let max_times = 5u8;
+    let mut err_times = 0u8;
+    let mut input = String::new();
+    // let mut word_so_far = String::new();
+    let mut word_so_far = Vec::new();
+    let mut unknow_word = HashSet::new();
+    let mut guessed_word = String::new();
+    // let mut guessed_word = HastSet::new();
+    for i in 0..secret_word_chars.len() {
+        word_so_far.push('-');
+        unknow_word.insert(secret_word_chars[i]);
+    }
+    loop {
+        // 清空历史输入
+        input.clear();
+
+        // 打印进度
+        println!("");
+        // println!("The word so far is {}", word_so_far);
+        println_word_so_far(&word_so_far);
+
+        // 打印历史输入
+        println!("You have guessed the following letters: {}", guessed_word);
+
+        // 打印剩余次数
+        println!("You have {} guesses left", max_times - err_times);
+
+        // 提示输入
+        println!("Please guess a letter:");
+
+        match io::stdin().read_line(&mut input) {                           // O(n)
+            Ok(n) => {
+                match n {
+                    // 只能输入单个字符 + 一个 0xA 换行字符
+                    2 => {
+                        let input_char = input.chars().nth(0).unwrap();
+
+                        // 记录历史
+                        guessed_word.push(input_char);
+                        
+                        /// case 1：猜对一个
+                        /// case 2：猜对全部
+                        /// case 3：没有猜对
+                        // match secret_word_chars.contains(&input_char) {  // O(n)
+                        //                                                      vs
+                        match unknow_word.contains(&input_char) {           // O(1)
+                            true => {
+                                for i in 0..secret_word_chars.len() {       // O(n)
+                                    if input_char == secret_word_chars[i] {
+                                        // word_so_far.remove(i);              // O(n)
+                                        // word_so_far.insert(i, input_char);  // O(n)
+                                        //                                         vs
+                                        word_so_far[i] = input_char;           // O(1)
+                                    }
+                                }
+
+                                // if word_so_far == secret_word {          // O(n)
+                                //                                              vs
+                                if unknow_word.is_empty() {                 // O(1)
+                                    println!("");
+                                    println!("Congratulations you guessed the secret word: {}!", secret_word);
+                                    break;
+                                }
+                            }
+                            false => {
+                                println!("Sorry, that letter is not in the word");
+                                // 记录错误次数
+                                err_times += 1;
+                                if err_times >= max_times {
+                                    println!("");
+                                    println!("No more try. :-(");
+                                    println!("The secret word is {}", secret_word);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    1 => {
+                        println!("Please input at least one letter.");
+                        continue;
+                    }
+                    _ => {
+                        println!("Please input only one letter.");
+                        continue;
+                    }
+                }
+            }
+            Err(error) => {
+                println!("Error: {error}");
+            }
+        }
+    }
+
+    fn println_word_so_far(word_so_far: &Vec<char>) {
+        print!("The word so far is ");
+        for c in word_so_far {
+            print!("{}", c);
+        }
+        println!("");
+    }
 }
